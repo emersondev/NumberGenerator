b1a90d3f7c95492e9c728575b5ad505cadf571e4
Emerson Novais <emersonnovais93@gmail.com>
2022-03-18 17:56:14 -0300

    Simple app for generate number
    Complete app

--- NumberGenerator/ViewController.swift
+++ NumberGenerator/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func generateNumber(_ sender: Any) {
+        var chooseNumber = arc4random_uniform(11)
+        resultLabel.text = String(chooseNumber)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
